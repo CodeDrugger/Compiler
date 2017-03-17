32bbd895580fd84c3c1f58ca4b05ba2e64c5183a
CodeDrugger <dangwj1996@gmail.com>
2017-03-17 23:14:58 +0800

    添加项目文件。

---
+++ Compiler/macro.h
@@ -0,0 +1,51 @@
+#pragma once
+//保留字
+#define		BREAK		1
+#define		CHAR		2
+#define		CONTINUE	3
+#define		ELSE		4
+#define		FOR			5
+#define		GOTO		6
+#define		IF			7
+#define		INT			8
+#define		RETURN		9
+#define		WHILE		10
+//类型
+#define		ID			11
+#define		CHAR		12
+#define		INT			13
+//运算符
+#define		PLUS		14 // +
+#define		MINUS		15 // -
+#define		MULTI		16 // *
+#define		RDIV		17 // /
+#define		MOD			0 // %
+#define		INC			0 // ++
+#define		DEC			0 // --
+#define		AND			0 // &&
+#define		OR			0 // ||
+#define		NOT			0 // !
+//比较符
+#define		EQ			0 // ==
+#define		LT			0 // <
+#define		GT			0 // >
+#define		LE			0 // <=
+#define		GE			0 // >=
+#define		NE			0 // != <>
+//括号
+#define		LS_BRAC		0 // (
+#define		RS_BRAC		0 // )
+#define		LM_BRAC		0 // [
+#define		RM_BRAC		0 // ]
+#define		LL_BRAC		0 // {
+#define		RL_BRAC		0 // }
+//其他符号
+#define		ASSIGN		0 // =
+#define		SEMIC		0 // ;
+#define		Q_MARK		0 // '
+#define		DQ_MARK		0 // "
+#define		B_SLANT		0 /* \ */
+#define		COMMA		0 // ,
+#define		POUND		0 // #
+#define		ADDRESS		0 // &
+#define		COLON		0 // :
